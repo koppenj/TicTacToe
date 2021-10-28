66cb85da1c345fa1f4a4e81df380aff8763da322
koppenj <joshuwakoppen@gmail.com>
2021-10-28 14:04:42 -0400

    Changing public facing API

--- script.js
+++ script.js
@@ -1,28 +1,60 @@
-const gameBoard = (() => {
+// Initialize game
+const game = (() => {
   const container = document.querySelector('#gameBoard');
-  let board = ['X', 'O', 'X',
-               'O', 'X', 'O',
-               'X', 'O', 'X']
-  for (let i = 0; i < board.length; i++) {
-    const tile = document.createElement('div');
-    tile.classList.add('tile');
-    tile.textContent = board[i];
-    container.appendChild(tile);
-  }
-return board;
+  const board = [' ', ' ', ' ',
+               ' ', ' ', ' ',
+               ' ', ' ', ' ']
+  const draw = (board) => {
+    for (let i = 0; i < board.length; i++) {
+      const tile = document.createElement('div');
+      tile.classList.add('tile');
+      tile.id = i;
+      tile.textContent = board[i];
+      container.appendChild(tile);
+    }
+  };
+  draw(board);
+return { container, board , draw };
 })();
 
+// Player FF
 const playerCreate = (name, marker) => {
   return {name, marker};
 };
+const Josh = playerCreate('Josh', 'X');
+const Amanda = playerCreate('Amanda', 'O');
 
-const clearBoard = () => {
-  console.log('hi mom')
-  let board = [];
-  return board;
+// Make tiles playable
+const play = () => {
+  let turnCounter = Josh;
+  let newBoard = [...game.board];
+
+  const tiles = document.querySelectorAll('.tile');
+  tiles.forEach((tile) => {
+    tile.addEventListener('click', (event) => {
+      newBoard[event.target.id] = `${turnCounter.marker}`;
+      game.container.replaceChildren();
+      game.draw(newBoard);
+
+    });
+  });
+
 }
 
-  const button = document.querySelector('#clear');
-  button.addEventListener('click', clearBoard);
+// Button functions
+const clearBoard = () => {
+  console.log('hi mom');
+}
+
+  const clearButton = document.querySelector('#clear');
+  clearButton.addEventListener('click', clearBoard);
+
+  const newGame = () => {
+    console.log('starting fresh');
+    play();
+  }
+
+  const newButton = document.querySelector('#newGame');
+  newButton.addEventListener('click', newGame);
 
 
