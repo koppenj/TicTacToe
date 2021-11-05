9b54029d2644339813dfb7fa411149b21869f063
koppenj <joshuwakoppen@gmail.com>
2021-11-05 13:09:27 -0400

    Build out marker placement. Added notification for turn alert.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const message = document.querySelector('#notifications');
 // Initialize game
 const game = (() => {
   const container = document.querySelector('#gameBoard');
@@ -25,25 +26,31 @@ const Josh = playerCreate('Josh', 'X');
 const Amanda = playerCreate('Amanda', 'O');
 
 // Make tiles playable
-const play = () => {
+const play = (() => {
   let turnCounter = Josh;
   let newBoard = [...game.board];
 
-  const tiles = document.querySelectorAll('.tile');
-  tiles.forEach((tile) => {
-    tile.addEventListener('click', (event) => {
-      newBoard[event.target.id] = `${turnCounter.marker}`;
-      game.container.replaceChildren();
-      game.draw(newBoard);
-
+  const battle = () => {
+    const tiles = document.querySelectorAll('.tile');
+    tiles.forEach((tile) => {
+      tile.addEventListener('click', (event) => {
+        newBoard[event.target.id] = `${play.turnCounter.marker}`;
+        game.container.replaceChildren();
+        game.draw(newBoard);
+        turnSwitch();
+        battle();
+      });
     });
-  });
-
-}
+  }
+  return { battle, newBoard, turnCounter };
+})();
 
 // Button functions
 const clearBoard = () => {
   console.log('hi mom');
+  play.newBoard = [...game.board];
+  game.container.replaceChildren();
+  game.draw(play.newBoard);
 }
 
   const clearButton = document.querySelector('#clear');
@@ -51,10 +58,22 @@ const clearBoard = () => {
 
   const newGame = () => {
     console.log('starting fresh');
-    play();
+    message.textContent = `${play.turnCounter.name}`+ `'s turn`;
+    play.battle();
   }
 
   const newButton = document.querySelector('#newGame');
   newButton.addEventListener('click', newGame);
 
+  const displayMessage = () => {
+    message.textContent = `${play.turnCounter.name}`+ `'s turn`;
+  }
 
+  const turnSwitch = () => {
+    if(play.turnCounter == Josh) {
+      play.turnCounter = Amanda;
+    } else {
+      play.turnCounter = Josh;
+    }
+    displayMessage();
+  }
